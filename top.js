fa0636893af0f4fac506ca4aa534a8f2b2b26011
Varun <71378746+varun570@users.noreply.github.com>
2021-05-03 17:19:58 +0530

    Add files via upload

---
+++ top.js
@@ -0,0 +1,13 @@
+var mybutton = document.getElementById("myBtn");
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+  if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
